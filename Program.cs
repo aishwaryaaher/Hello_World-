fb3dcaec47df1c7243c59d8ca7ef734927dde0aa
aishwaryaaher <91543285+aishwaryaaher@users.noreply.github.com>
2021-11-13 09:57:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello_World_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Hello World!");
+            Console.ResetColor();
+
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Welcome To My 1st Project ");
+            Console.WriteLine("");
+            Console.ResetColor();
+
+
+        }
+    }
+}
